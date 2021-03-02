1424ee6e46121dfb08bd295c6f6a249bfe6ea856
Darius Džervus <d.dzervus@verifo.com>
2021-03-02 08:58:15 +0700

    authorization

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,6 +8,7 @@ interface VerifoConfig {
   api_secret:string,
   api_version:string,
   api_url:string;
+  api_domain:string;
 }
 
 let verifoConfig:VerifoConfig;
@@ -35,63 +36,110 @@ private getHmacHash(message:any) {
         const CLIENT_KEY = verifoConfig.api_key
         const SECRET_KEY = verifoConfig.api_secret;
         const VERSION = verifoConfig.api_version.toUpperCase();
-          
+
         const hmacDigest = crypto.createHmac('sha512',SECRET_KEY).update(message).digest('hex');
-        
+
         let headerValue = VERSION+'-HMAC-SHA512 Key='+CLIENT_KEY+',Signature='+hmacDigest;
-    
+
         return headerValue;
-        
+
     } catch (error) {
 
         console.log('the error '+JSON.stringify(error))
-        
+
     }
-    
+
 }
 
 
 async createCompanyProfile(dto:any){
 
     try {
-
         const data = {
-            
             name:dto.name,
-            registrationCode:dto.registration_number,
-            registrationDate:dto.registration_date,
-            registrationCountry:dto.registration_country_code,
-            legalForm:dto.legal_form,
+            registrationCode:dto.registrationCode,
+            registrationDate:dto.registrationDate,
+            registrationCountry:dto.registrationCountry,
+            legalForm:dto.legalForm,
             phone:dto.phone,
             email:dto.email
         }
 
+        const dataStringify = JSON.stringify(data);
+
         const nonce = this.getNonce();
         const timestamp = this.getTimestamp();
+        const queryString = `/api/${verifoConfig.api_version}/company?nonce=${nonce}&timestamp=${timestamp}`
 
-        const queryString = `nonce=${nonce}&timestamp=${timestamp}`
-        
-        const message = queryString+'|'+data;
+        const message = queryString+'|'+ dataStringify;
 
         const header:any = this.getHmacHash(message);
 
-        console.log('the data '+JSON.stringify(data));
-        console.log('the timestamp '+timestamp);
-        console.log('the nonce '+nonce);
-        console.log('the header '+header);
+        console.log('the data: '+dataStringify);
+        console.log('the timestamp: '+timestamp);
+        console.log('the nonce: '+nonce);
+        console.log('the header: '+header);
+        console.log('the url: '+`${verifoConfig.api_url}`+queryString)
 
-        console.log('the url '+`${verifoConfig.api_url}/api/${verifoConfig.api_version}/company?nonce=${nonce}&timestamp=${timestamp}`)
+        // const post= bent('POST','json',200);
+        // const response = await post(`${verifoConfig.api_url}/api/${verifoConfig.api_version}/company?nonce=${nonce}&timestamp=${timestamp}`, data2, headesr);
 
-        const post= bent('POST','json',200);
+        // const https = require('https')
+        // const options = {
+        //     hostname: verifoConfig.api_domain,
+        //     port: 443,
+        //     path: queryString,
+        //     method: 'POST',
+        //     headers: {
+        //         'Authorization': header,
+        //         'Content-Type': 'application/json',
+        //         'Content-Length': dataStringify.length
+        //     }
+        // }
+        //
+        // const req = https.request(options, res => {
+        //     console.log(`statusCode: ${res.statusCode}`)
+        //
+        //     res.on('data', d => {
+        //         console.log(`response: ${d}`)
+        //     })
+        // });
+        //
+        // req.on('error', error => {
+        //     console.error(error)
+        // })
+        //
+        // req.write(dataStringify)
+        // req.end();
 
-        const response = await post(`${verifoConfig.api_url}/api/${verifoConfig.api_version}/company?nonce=${nonce}&timestamp=${timestamp}`, data,header);
-                 
-        return [null,response]
-        
+
+
+        const axios = require('axios');
+
+        let config = {
+            headers: {
+                'Authorization': header,
+                'Content-Type': 'application/json',
+                'Content-Length': dataStringify.length
+            }
+        }
+
+        axios.post(verifoConfig.api_url+queryString, data, config)
+            .then(res => {
+                console.log(res.data.data);
+                console.log(`statusCode `+res.status);
+            })
+            .catch(error => {
+                console.error(error.response)
+            })
+
+
+        return [null,null]
+        // return [null,response]
+
     } catch (error) {
 
-        return [error,null] 
+        return [error,null]
     }
-
 }
 }
--- src/config/verifo.ts
+++ src/config/verifo.ts
@@ -6,6 +6,6 @@ export default registerAs('verifo', () => ({
   api_key: process.env.VERIFO_API_KEY,
   api_secret:process.env.VERIFO_API_SECRET,
   api_version:process.env.VERIFO_API_VERSION,
-  api_url:process.env.VERIFO_API_URL
-
+  api_url:process.env.VERIFO_API_URL,
+  api_domain:process.env.VERIFO_API_DOMAIN
 }));
